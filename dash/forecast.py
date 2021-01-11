d295cb956ec01d8c645ce430597085d88a963b70
Anthony Yoskovich <anthony.yoskovich@gmail.com>
2021-01-11 18:14:22 -0500

    Adding dashboard module

---
+++ dash/forecast.py
@@ -0,0 +1,25 @@
+
+
+class Future():
+  """ Class for the future """
+
+  def __init__(self, data):
+    self.data = data
+
+
+  def forecast(self, how, amt):
+    """ Forecast by using the how method 
+    SMA, others...
+    """
+    pass
+
+
+  def how_likely(self, scen):
+    """ How likely is scen? 
+    
+    """
+    pass
+
+
+
+
